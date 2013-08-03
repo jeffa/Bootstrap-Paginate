fd6a81f95a3bb3831cc81054319fd9904d1a56c1
jeffa <jeffa@unlocalhost.com>
2013-08-03 14:12:39 -0400

    added ajax framework

--- lib/Bootstrap/Paginate.pm
+++ lib/Bootstrap/Paginate.pm
@@ -4,7 +4,22 @@ use Dancer ':syntax';
 our $VERSION = '0.1';
 
 get '/' => sub {
-    template 'index';
+    my %vars = ();
+
+    template index => \%vars;
+};
+
+get '/results' => sub {
+    my %vars = ();
+
+    $vars{results} = [
+        { qw( foo a bar a baz a qux a ) },
+        { qw( foo b bar b baz b qux b ) },
+        { qw( foo c bar c baz c qux c ) },
+        { qw( foo d bar d baz d qux d ) },
+    ];
+
+    template results => \%vars;
 };
 
 true;
